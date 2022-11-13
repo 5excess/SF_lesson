b5c5e68cc882991eb3e7b94e235a1a2b1e9c8dfd
5excess <janosh3@gmail.com>
2022-11-14 00:15:39 +0300

    first commit

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Practice </title>
+    <title>PHP Practice 1</title>
     <link rel="stylesheet" href="style.css" />
 </head>
 <body>
@@ -13,7 +13,7 @@
         <div class="header">     
                <?php include 'logo.inc.php' ?>         
                <?php include 'menu.inc.php' ?>	   
-        </div>       
+        </div>
      
         <div class="about_me">
          
